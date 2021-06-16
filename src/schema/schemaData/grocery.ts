ab1e47b1beb211a41c069c8aa3b4e15eb3b6a237
Tejesh <tejesh.allampati@zemosolabs.com>
2021-06-16 16:30:34 +0530

    Fixed TODO

--- src/schema/schemaData/grocery.ts
+++ src/schema/schemaData/grocery.ts
@@ -1,7 +1,6 @@
 import {getFromObjectArray} from "../../helpers/getFromArray";
 import { getAllGroceries, getGroceryById, getGroceryByName } from "../../service/GroceryService";
 
-//TODO: Add groceryByCategory(category: string): Grocery to schema
 export const GroceryQueries = `
     grocery(name: String!): Grocery
     groceries: [Grocery!]
@@ -14,10 +13,13 @@ export const GroceryResolvers = {
         await getGroceryByName(name).then(data => result=data);
         return result;
     },
+
     groceryById: async function(_:unknown, { id } : { id:number }) {
         let result = {};
         await getGroceryById(id).then(data=> result = data);
         return result;
     },
+
     groceries: () => getAllGroceries(),
+
 }
